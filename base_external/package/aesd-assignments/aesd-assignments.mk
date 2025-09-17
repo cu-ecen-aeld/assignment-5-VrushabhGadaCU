782a3a437da997a6949e18a81cab871a25b5e81d
Vrushabh <Vrushabh.Gada@colorado.edu>
2025-09-16 22:02:36 -0600

    update the commit id

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '3f4633419d9b64c1d8c3b927f1d75b42846ebab8'
+AESD_ASSIGNMENTS_VERSION = 'd824f5dae56d366952f759a7d2e9907fcabc15e7'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
