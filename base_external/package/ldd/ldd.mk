ed0e02bb4503a0b0bbc33ac611bc2ee6bd322bbc
Vrushabh <Vrushabh.Gada@colorado.edu>
2025-10-12 01:59:35 -0600

    modified the scirpt to run the module

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -9,16 +9,21 @@ LDD_VERSION = 87cc39d561a7e97843e2a21691b567446d6c66fe
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-VrushabhGadaCU.git
 LDD_SITE_METHOD = git
 
-# The subdirectories inside your repo that contain kernel modules
+LDD_GIT_SUBMODULES = YES
 LDD_MODULE_SUBDIRS = misc-modules scull
 
-
-# Install helper scripts for our init script (paths used by S98lddmodules)
-define LDD_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/misc-modules/module_load   $(TARGET_DIR)/usr/local/bin/module_load
-	$(INSTALL) -D -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/local/bin/module_unload
-	$(INSTALL) -D -m 0755 $(@D)/scull/scull_load           $(TARGET_DIR)/usr/local/bin/scull_load
-	$(INSTALL) -D -m 0755 $(@D)/scull/scull_unload         $(TARGET_DIR)/usr/local/bin/scull_unload
+# Build
+define LDD_BUILD_CMDS
+	$(MAKE) -C $(LINUX_DIR) M=$(@D)/misc-modules $(LINUX_MAKE_FLAGS) EXTRA_CFLAGS="-I$(@D)/include" modules
+	$(MAKE) -C $(LINUX_DIR) M=$(@D)/scull $(LINUX_MAKE_FLAGS) EXTRA_CFLAGS="-I$(@D)/include" modules
 endef
+
+# Install 
+define LDD_INSTALL_TARGET_CMDS
+    mkdir -p $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra
+	$(INSTALL) -m 0755 $(@D)/misc-modules/*.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra
+	$(INSTALL) -m 0755 $(@D)/scull/*.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra
+endef
+
 $(eval $(kernel-module))
 $(eval $(generic-package))
